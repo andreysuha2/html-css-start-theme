b3c0ce6098ff13fe5f13a32064d1a04edaf57b9b
Andrey Suhenko <andreysuha2@gmail.com>
2019-05-02 17:34:25 +0300

    remove dml

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@
 import '@/assets/sass/main.scss';
 //import global libs
 import dl from 'lib/js/devLogger';
-import dml from 'lib/js/dml';
 // set libs to global scope
 window.dl = dl;
-window.$dml = dml;
 // import global constants
 import {isDev} from "lib/js/devLogger";
 // set global constants
